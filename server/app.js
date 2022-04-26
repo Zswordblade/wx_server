b0945426eb9d7ab24e331e35290a20e529d5c34e
Zzj <2584970094@qq.com>
2022-04-26 10:15:17 +0800

    备份

--- server/app.js
+++ server/app.js
@@ -4,12 +4,24 @@ const router = require("./router")
 const bodyParser = require("body-parser");
 const multer = require('multer')
 
+const http = require('http').Server(app)
+// const io = require("socket.io")(http)
+
+
+// io.on('connection', socket => {
+//     socket.emit('success', '连接到服务器')
+//     socket.on('disconnect', () => {
+//         io.emit('quit', socket.id)
+//     })
+// })
+
+
 app.use(multer({ dest: './public/' }).any())// 通过配置multer的dest属性， 将文件储存在项目下的tmp文件中
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use('/public', express.static('public'));
 app.use("/api",router)
 
-app.listen(3200,() =>{
+http.listen(3200,() =>{
     console.log("server run at port http://127.0.0.1:3200/");
 })
--- server/router.js
+++ server/router.js
@@ -20,7 +20,7 @@ const conn = mysql.createConnection({
   })
   // 测试连接
   conn.connect(err=>{
-    console.log(err,'如果为null 就是连接成功');
+    console.log(err,'左边为null 就是连接成功');
   })
 
 router.get("/homehot",(req,res)=>{
@@ -54,33 +54,81 @@ router.post('/uploadPicture', function(req, res){
     fs.renameSync('./public/'+oldFileName,newFileName)
     res.send(newFileName)
 });
-
+// 记录用户信息
+router.post('/setUserInfo',(req,res)=>{
+    console.log(req.body)
+    let { name, avr_url, userID } = req.body
+    console.log(`新用户的name:${name}，头像地址：${avr_url},userID:${userID}`)
+    if  (userID&&name&&avr_url) {
+        let sqlStr = `SELECT * FROM user WHERE userID='${userID}'`//先判断是否是新用户
+        conn.query(sqlStr, (err, results) => {
+            if (err) {
+                res.json({msg:'false',err})
+            } else if (!results.length) {
+                sqlStr = `INSERT INTO user (userID, name, avr_url) VALUES('${userID}', '${name}', '${avr_url}')`
+                conn.query(sqlStr,(err) => {
+                    if (err) {
+                        res.json({msg:'false',err})
+                    } else 
+                        res.json({msg:'true'})
+                })
+            } else 
+                res.json({msg:'true'})
+        })
+    } else 
+        res.json({err:'没传openid'})
+})
+//添加商品
 router.post('/addGood',(req,res)=>{
     console.log(req.body)
     let {title,price,token,pic_url,sort} = req.body
     let sqlStr =  `INSERT INTO goods ( title, price, userID,pic_url,sort )VALUES('${title}',${price},'${token}','${pic_url}','${sort}')`
        //执行mysql 语句
     conn.query(sqlStr,(err)=>{
-           if(err){
-             res.json({msg:'false',err})
-           }else{
-             res.json({msg:'true'})
-             console.log(err,'如果为null，sql语句执行成功')
-           }
-           
+        if(err){
+            res.json({msg:'false',err})
+        }else{
+            res.json({msg:'true'})
+            console.log(err,'如果为null，sql语句执行成功')
+        }
+    })
+})
+//修改商品信息
+router.post('/updateGoods',(req, res) => {
+    console.log(req.body)
+    let {id,title,price,pic_url,sort} = req.body 
+    console.log(pic_url)
+    let sqlStr = `UPDATE goods SET title='${title}',price='${price}', pic_url='${pic_url}', sort='${sort}' WHERE id = '${id}'`
+    conn.query(sqlStr, (err) => {
+        if (err) {
+            res.json({msg:'false', err})
+            console.log(err)
+        } else {
+            res.json({msg:'true'})
+        }
     })
 })
 //首页楼层数据
 router.get("/home/floordata",(req,res)=>{
-    let sql = `SELECT * FROM goods `
+    const pageNum = req.query.pageNum * 8
+    console.log('pageNum:' + pageNum +' '+ 'sort:' + req.query.sort)
+    let sql = `SELECT * FROM goods WHERE selled = '0' AND audit_status = '1' ORDER BY id LIMIT 8 OFFSET ${pageNum}`
+    if (req.query.sort) {
+        sql = `SELECT * FROM goods WHERE selled = '0' AND audit_status = '1' AND sort = '${req.query.sort}' ORDER BY id LIMIT 8 OFFSET ${pageNum}`
+    }
     conn.query(sql,(err,results)=>{
-        res.send(results)
+        if(err) {
+            console.log(err)
+            res.send({msg:'false',err})
+        } else {
+            res.send({msg:'true',results})
+        }
     })
 })
 //获取商品详情
 router.get('/goodsDetail',(req,res)=>{
     const {id} = req.query
-    let sql = `SELECT * FROM goods WHERE id=${id}`
+    let sql = `SELECT * FROM goods g LEFT JOIN user u ON g.userID=u.userID WHERE g.id=${id}`
     conn.query(sql,(err,results)=>{
         res.send(results)
     })
@@ -97,7 +145,21 @@ router.get('/collectGoods',(req,res)=>{
           res.json({msg:'true'})
           console.log(err,'收藏商品成功')
         }
-        
+    })
+})
+//取消收藏
+router.get('/unCollectGoods',(req,res)=>{
+    const {goodsid,token} = req.query
+    console.log(`goodsid:${goodsid}  token:${token}`)
+    let sqlStr =  `DELETE FROM favorites WHERE userID = '${token}' AND goodsID = '${goodsid}'`
+    //执行mysql 语句
+    conn.query(sqlStr,(err)=>{
+        if(err){
+          res.json({msg:'false',err})
+          console.log(err)
+        }else{
+          res.json({msg:'true'})
+        }
     })
 })
 // 获取收藏
@@ -109,11 +171,25 @@ router.get('/my/favoriGoods',(req,res)=>{
         res.send(results)
     })
 })
+//获取用户个人的商品
+router.get('/my/userGoods',(req, res) => {
+    const { token, pageNum } = req.query
+    console.log('获取用户个人的商品--' + token + ' ' + pageNum)
+    let sql = `SELECT * FROM goods WHERE goods.userID='${token}' ORDER BY id LIMIT 8 OFFSET ${pageNum*8}`
+    conn.query(sql, (err, results) => {
+        if (err) {
+            res.send({msg:'false',err})
+            console.log(err)
+        } else {
+            res.send({msg:'true', results})
+        }
+    })
+})
 //添加地址
 router.post('/my/myAddress',(req,res)=>{
     console.log(req.body)
     let {name,id,phone,address} = req.body
-    let sqlStr =  `INSERT INTO address ( user_name, user_id,phone,address )VALUES('${name}','${id}','${phone}','${address}')`
+    let sqlStr =  `INSERT INTO address ( user_name, user_id, phone, address )VALUES('${name}','${id}','${phone}','${address}')`
        //执行mysql 语句
     conn.query(sqlStr,(err)=>{
            if(err){
@@ -135,5 +211,203 @@ router.get('/getAddress',(req,res)=>{
         res.send(results)
     })
 })
-
+//////////////////////
+//订单
+/////////////////////
+// 创建订单
+router.post('/makeOrder',(req,res)=>{
+    // console.log(req.body)
+    let {customer,order_status,goodsID} = req.body
+    let sqlStr =  `INSERT INTO orders ( customer,order_status,goodsID)VALUES('${customer}','${order_status}','${goodsID}')`
+    let sqlStr1 = `UPDATE goods SET selled='1' WHERE id=${goodsID}`
+       //执行mysql 语句
+    conn.query(sqlStr,(err)=>{
+           if(err){
+             res.json({msg:'false',err})
+             console.log(err)
+           }else{
+             conn.query(sqlStr1,(err)=>{
+                 if(err){
+                    res.json({msg:'false',err})
+                    console.log(err)
+                 } else {
+                    res.json({msg:'true'})
+                 }
+             })
+           }      
+    })
+})
+//获取订单
+router.get('/home/getOrder',(req,res)=>{
+    const {token} = req.query
+    let sql = `SELECT * FROM (orders o LEFT JOIN goods g ON o.goodsID=g.id) LEFT JOIN user ON user.userID=g.userID WHERE o.customer='${token}'`
+    conn.query(sql,(err,results)=>{
+        if(err)
+            console.log(err)
+        res.send(results)
+    })
+})
+//用户评价
+router.post('/estimate',(req,res)=>{
+    console.log(req.body)
+    let {estimate,orderID,customer,seller,rate,datetime} = req.body
+    let sqlStr =  `INSERT INTO evaluate ( estimate,orderID,customer,seller,rate,create_time )VALUES('${estimate}','${orderID}','${customer}','${seller}','${rate}','${datetime}')`
+    conn.query(sqlStr,(err)=>{
+        if(err){
+            res.json({msg:'false',err})
+        }else{
+            sqlStr = `UPDATE orders SET estimate_status = '1' WHERE id = '${orderID}'`
+            conn.query(sqlStr,(errs) => {
+            if(errs){
+                res.json({msg:'false',errs})
+            }else{
+                res.json({msg:'true'})
+            }
+            })
+        }  
+    })
+})
+router.get('/getEstimate',(req,res)=>{
+    const {seller_token} = req.query
+    let sql = `SELECT * FROM evaluate LEFT JOIN user on user.userID = evaluate.customer WHERE evaluate.seller='${seller_token}'`
+    conn.query(sql,(err,results) => {
+        if(err) {
+            res.send({err,msg:'false'})
+            console.log(err)
+        } else {
+            res.send({results,msg:'true'})
+        }
+    })
+})
+///////////后台//////////
+// 管理员登录
+router.post('/user/login',(req,res)=>{
+    console.log(req.body)
+    let {username,password} = req.body
+    let sql = `SELECT * FROM manager WHERE name='${username}' AND password='${password}'`
+    conn.query(sql,(err,results)=>{
+        if(err) {
+            console.log(err)
+            res.send({msg:'false',err})
+        } else if (results.length<=0)
+            res.send({msg:'false',err:'账号密码有误'})
+        else
+            res.send({msg:'true',token:'runaway'})
+    })
+})
+// 后台商品列表
+router.get('/admin/getGoods',(req,res)=>{
+    const {pageNum} = req.query
+    console.log('请求页' + pageNum)
+    let sql = `SELECT * FROM goods g LEFT JOIN user ON user.userID=g.userID ORDER BY g.id DESC LIMIT 5 OFFSET ${pageNum*5-5}`
+    conn.query(sql,(err,result1)=>{
+        if(err) {
+            res.send({err,msg:'false'})
+            console.log(err)
+        } else {
+            sql = `SELECT * FROM goods`
+            conn.query(sql, (err, result2) => {
+                if (err) {
+                    res.send({err,msg:'false'})
+                    console.log(err)
+                } else {
+                    res.send({ msg:'true', rows:result1, count: result2.length||0 })
+                }
+            })
+        }
+    })
+})
+// 后台商品上下架
+router.get('/admin/checkGoods',(req,res)=>{
+    const {audit_status, id} = req.query
+    console.log(req.query)
+    let sql = `UPDATE goods SET  audit_status='${audit_status}' where id='${id}'`
+    conn.query(sql,(err)=>{
+        if(err){
+            console.log(err)
+            res.send({msg:'false',err})
+        } else {
+            res.send({msg:'true'})
+        }
+    })
+})
+//后台订单
+router.get('/admin/getOrder',(req,res)=>{
+    const {pageNum} = req.query
+    let sql = `SELECT * FROM orders o LEFT JOIN user u ON o.customer=u.userID ORDER BY o.id DESC LIMIT 5 OFFSET ${pageNum*5-5}`
+    conn.query(sql,(err,results1)=>{
+        if(err) {
+            console.log(err)
+            res.send({msg:'false',err})
+        } else {
+            sql = `SELECT * FROM orders o LEFT JOIN user u ON o.customer=u.userID`
+            conn.query(sql, (err, results2) => {
+                if (err) {
+                    console.log(err)
+                    res.send({msg:'false',err})
+                } else {
+                    res.send({ rows:results1, msg:'true', count:results2.length||0 })
+                }
+            })
+        }
+    })
+})
+// 获取评价
+router.get('/admin/getFeedBack',(req,res)=>{
+    const {pageNum} = req.query
+    let sql = `SELECT * FROM evaluate e LEFT JOIN user u ON e.customer=u.name`
+    conn.query(sql, (err, results1) => {
+        if(err) {
+            console.log(err)
+            res.send({msg:'false', err})
+        } else {
+            sql = `SELECT * FROM evaluate e LEFT JOIN user u ON e.customer=u.name ORDER BY e.eva_id DESC LIMIT 5 OFFSET ${pageNum*5-5}`
+            conn.query(sql, (err, results2) => {
+                if (err) {
+                    console.log(err)
+                    res.send({msg:'false', err})
+                } else {
+                    res.send({ msg: 'true', rows: results2, count: results1.length || 0 })
+                }
+            })
+        }
+    })
+})
+// 删除评价
+router.get('/admin/deleEstimate',(req,res)=>{
+    const {id} = req.query
+    let sql = `DELETE FROM evaluate WHERE eva_id=${id}`
+    conn.query(sql,(err)=>{
+        if(err) {
+            console.log(err)
+            res.send({msg:'false',err})
+        } else {
+            res.send({msg:'true'})
+        }
+    })
+})
+//获取用户表
+router.get('/admin/getUser',(req,res)=>{
+    let sql = `SELECT * FROM user`
+    conn.query(sql,(err,result)=>{
+        if(err) {
+            console.log(err)
+            res.send({msg:'false',err})
+        } else {
+            res.send({msg:'true',rows:result})
+        }
+    })
+})
+//禁用用户
+router.get('/admin/banUser',(req,res)=>{
+    const {id,banned} = req.query
+    let sql = `UPDATE user SET banned=${banned} WHERE u_id=${id}`
+    conn.query(sql,(err)=>{
+        if(err) {
+            console.log(err)
+            res.send({msg:'false',err})
+        } else 
+            res.send({msg:'true'})
+    })
+})
 module.exports = router;
